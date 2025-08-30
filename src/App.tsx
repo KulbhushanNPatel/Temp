8cd4f827a630bfa4eb9eb2984ef27e1785d8f20c
KulbhushanNPatel <91859898+KulbhushanNPatel@users.noreply.github.com>
2025-08-30 19:15:46 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Welcome from "./pages/Welcome";
+import FoodLookup from "./pages/FoodLookup";
+import AIDietPlanner from "./pages/AIDietPlanner";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/lookup" element={<FoodLookup />} />
+            <Route path="/planner" element={<AIDietPlanner />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AnimatePresence>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
